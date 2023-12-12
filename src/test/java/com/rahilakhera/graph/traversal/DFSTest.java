ccf4f19d43044d4b96ebb5a8d30e444de0b03203
RAHIL KHERA <rahilakhera@gmail.com>
2023-12-12 23:16:23 +0530

    Added test cases to test dfs for different graphs.

--- src/test/java/com/rahilakhera/graph/traversal/DFSTest.java
+++ src/test/java/com/rahilakhera/graph/traversal/DFSTest.java
@@ -4,37 +4,98 @@ import java.util.Arrays;
 import java.util.List;
 
 import com.rahilakhera.graph.graph.Graph;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
 
-public class DFSTest  extends TestCase {
-      
-    private Graph graph;
-    private DFS dfsTraversal;
+public class DFSTest {
 
-    public  DFSTest() {
-        super ("DFS Test");
-        graph = new Graph();
+    @Test
+    public void testBasicCase() {
+        Graph graph = new Graph();
         graph.addEdge(1, 2);
         graph.addEdge(1, 3);
         graph.addEdge(2, 4);
         graph.addEdge(2, 5);
         graph.addEdge(3, 6);
-        dfsTraversal = new DFS(graph);
+
+        DFS dfs = new DFS(graph);
+        List<Integer> result = dfs.dfs(1);
+
+        List<Integer> expected = Arrays.asList(1, 2, 4, 5, 3, 6);
+        assertEquals(expected, result);
     }
 
-    public static Test suite()
-    {
-        return new TestSuite( DFSTest.class );
+    @Test
+    public void testDisconnectedGraph() {
+        Graph graph = new Graph();
+        graph.addEdge(1, 2);
+        graph.addEdge(2, 3);
+        graph.addEdge(4, 5);
+
+        DFS dfs = new DFS(graph);
+        List<Integer> result = dfs.dfs(1);
+
+        List<Integer> expected = Arrays.asList(1, 2, 3);
+        assertEquals(expected, result);
     }
 
-    public void testBasicDFS()
-    {
-        List<Integer> nodes = dfsTraversal.dfs(1);
-        List<Integer> expected =  Arrays.asList(1,2,4,5,3,6);
-        assertEquals(expected, nodes);
+    @Test
+    public void testCycleDetection() {
+        Graph graph = new Graph();
+        graph.addEdge(1, 2);
+        graph.addEdge(2, 3);
+        graph.addEdge(3, 1);
+        graph.addEdge(3, 4);
+
+        DFS dfs = new DFS(graph);
+        List<Integer> result = dfs.dfs(1);
+
+        List<Integer> expected = Arrays.asList(1, 2, 3, 4);
+        assertEquals(expected, result);
     }
 
+    @Test
+    public void testDirectedGraph() {
+        Graph graph = new Graph(true);
+        graph.addEdge(1, 2);
+        graph.addEdge(2, 3);
+        graph.addEdge(2, 4);
+        graph.addEdge(3, 5);
+
+        DFS dfs = new DFS(graph);
+        List<Integer> result = dfs.dfs(1);
+
+        List<Integer> expected = Arrays.asList(1, 2, 3, 5, 4);
+        assertEquals(expected, result);
+    }
+
+    @Test
+    public void testComplexGraphStructure() {
+        Graph graph = new Graph();
+        graph.addEdge(1, 2);
+        graph.addEdge(1, 3);
+        graph.addEdge(2, 4);
+        graph.addEdge(2, 5);
+        graph.addEdge(3, 6);
+        graph.addEdge(4, 7);
+        graph.addEdge(5, 8);
+
+        DFS dfs = new DFS(graph);
+        List<Integer> result = dfs.dfs(1);
+
+        List<Integer> expected = Arrays.asList(1, 2, 4, 7, 5, 8, 3, 6);
+        assertEquals(expected, result);
+    }
+
+    @Test
+    public void testEmptyGraph() {
+        Graph graph = new Graph();
+
+        DFS dfs = new DFS(graph);
+        List<Integer> result = dfs.dfs(1);
+
+        List<Integer> expected = Arrays.asList();
+        assertEquals(expected, result);
+    }
 }
